fb1e33dd769da3d46ace3912cd914dad6106c051
shivgirja <amitsrivastava115@yahoo.com>
2022-01-22 18:57:27 +0530

    Initial Commit

---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Amit Shrivastava on 22/01/22.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
